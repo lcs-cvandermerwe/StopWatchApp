9e3ec97cb80448db08f9bd3c33ae326b605428ca
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2023-11-05 18:29:17 -0500

    Created CircleButtonView as a helper to make ContentView shorter.

---
+++ StopWatchApp/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  StopWatchApp
+//
+//  Created by Cari van der Merwe on 2023-11-05.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
