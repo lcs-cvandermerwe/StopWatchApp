ff0f889115de6b946058460c2691f895a19a80da
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2023-11-05 09:45:32 -0500

    Initial Commit

---
+++ StopWatchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StopWatchApp
+//
+//  Created by Cari van der Merwe on 2023-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StopWatchApp/StopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchAppApp.swift
+//  StopWatchApp
+//
+//  Created by Cari van der Merwe on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
