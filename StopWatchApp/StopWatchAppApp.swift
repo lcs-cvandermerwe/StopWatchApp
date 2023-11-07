941dd18ba8bdb9f175b974c02ca818d007376e26
Cari van der Merwe <cvandermerwe@lcs.on.ca>
2023-11-07 14:53:17 -0500

    made the selected tab item default to the stopwatch

--- StopWatchApp/StopWatchAppApp.swift
+++ StopWatchApp/StopWatchAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct StopWatchAppApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView {
+            TabView(selection: Binding.constant(3)) {
                 
                 Text("World Clock")
                     .tabItem {
